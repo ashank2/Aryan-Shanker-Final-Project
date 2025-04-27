fdfaa464bf4ca384af869c7a5fbc2784e3dd96e0
Aryan Shanker <ashanker2@babson.edu>
2025-04-27 18:46:14 -0400

    ML Libraries & Functions

---
+++ ml_functions.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------
+# Machine Learning Library Stack & Analytics Functions:
+#-------------------------------------------------------
+# pandas: For data manipulation and analysis, especially for Tabular Data. 
+import pandas as pd
+
+# numpy: For efficient numerial computations.
+import numpy as np
+
+# scikit-learn: For classic machine learning algorithms, model selection, and pre-processing.
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import classification_report, accuracy_score
+
+# matplotlib: For visualizing data and results (ex: in chart & graphical forms).
+import matplotlib.pyplot as plt
+
+def example_ml_function():
+    """
+    Example doc strings for Machine Learning Functions.
+    """
+    pass
+
